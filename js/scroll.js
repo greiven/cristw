c8228f5aec858fa33873655922f5d1158d04844e
Your Name <gbmurillo87@gmail.com>
2023-11-26 14:03:42 -0800

    web

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+  document.querySelector('.nav-menu').classList.toggle("show");
+
+});
+
+ScrollReveal().reveal('#carouselExampleIndicators1', { delay: 200});
+ScrollReveal().reveal('.card-header', { delay: 500 });
+ScrollReveal().reveal('.card', { delay: 200 });
+ScrollReveal().reveal('.row', { delay: 200 });
+ScrollReveal().reveal('.text-center', { delay: 300 });
+ScrollReveal().reveal('.navbar-nav sm-icons', { delay: 300 });
+ScrollReveal().reveal('.col-lg-4', { delay: 300 });
+ScrollReveal().reveal('.h1', { delay: 300 });
+ScrollReveal().reveal('.card-img-top img-thumbnail img-fluid', { delay: 300 });
+
+
+
+///////scroll top btn/////////
+
+
+
